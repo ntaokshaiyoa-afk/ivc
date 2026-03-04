99dd078c0cbcee2b0bcc5d568154aba8db023925
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-03-04 22:11:26 +0900

    github pages用設定

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/ivc/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
