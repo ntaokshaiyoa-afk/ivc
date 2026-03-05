50e7f5460db486beeced1c513a61a033a51a25e3
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-03-05 21:19:27 +0900

    Update oxipng.ts

--- src/codecs/oxipng.ts
+++ src/codecs/oxipng.ts
@@ -3,7 +3,7 @@ import { encode } from "@jsquash/png"
 export async function encodeOxiPNG(imageData: ImageData) {
 
   const result = await encode(imageData, {
-    level: 2
+quality:40
   })
 
   return result
