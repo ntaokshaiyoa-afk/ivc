29b2fe78789b39087ad30472cc11419c9b7acc7f
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-03-10 12:35:43 +0900

    Update imageCompressor.ts

--- src/services/imageCompressor.ts
+++ src/services/imageCompressor.ts
@@ -2,7 +2,11 @@ import { compressImageWorker } from './compressionWorker'
 
 import { detectFormat } from './compressionRouter'
 
-export async function compressImage(file: File, quality: number) {
+export async function compressImage(
+  file: File,
+  quality: number,
+  codec: string
+) {
   const format = detectFormat(file)
 
   return compressImageWorker(file, quality, format)
