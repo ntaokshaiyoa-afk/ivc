b4fa54a168c52b8f6a7f29e4152fdda8a49f8d64
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-04-07 08:54:33 +0900

    Update OfficeJobCard.tsx

--- src/components/jobs/OfficeJobCard.tsx
+++ src/components/jobs/OfficeJobCard.tsx
@@ -94,6 +94,17 @@ export function OfficeJobCard({ job, onChangeSettings, onRecompress }: Props) {
                 }
               />
 
+              <button
+  onClick={() => {
+    const next = { ...settings.overrides }
+    delete next[img.path]
+
+    onChangeSettings(job.id, { overrides: next })
+  }}
+>
+  AUTOに戻す
+</button>
+
               {/* 再圧縮 */}
               <button
                 onClick={() => onRecompress(job.id)}
