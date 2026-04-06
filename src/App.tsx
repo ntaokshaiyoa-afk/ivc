f83950c0b3ce40349750281420cf217b0e8ab803
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-04-06 22:49:30 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -172,6 +172,14 @@ export default function App() {
     },
     [],
   )
+  onChangeSettings(job.id, {
+  officeOverrides: {
+    [img.path]: {
+      format: img.format,
+      quality: img.quality,
+    },
+  },
+})
 
   const compressAll = async () => {
     setIsProcessing(true)
